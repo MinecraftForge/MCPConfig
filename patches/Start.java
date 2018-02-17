141a480d777d665c300d08c95bf7948675db2667
LexManos <LexManos@gmail.com>
2018-02-16 21:06:55 -0800

    Initial commit with snapshot 17w47a data. Patches will change as I am working on FernFlower to make the decompile better.

---
+++ patches/Start.java
@@ -0,0 +1,25 @@
+import java.io.File;
+import java.lang.reflect.Field;
+import java.util.Arrays;
+
+import net.minecraft.client.main.Main;
+
+public class Start
+{
+    public static void main(String[] args)
+    {
+        /* 
+         * start minecraft game application
+         * --version is just used as 'launched version' in snoop data and is required
+         * Working directory is used as gameDir if not provided
+         */
+        Main.main(concat(new String[]{"--version", "mcp", "--accessToken", "0", "--assetsDir", "assets", "--assetIndex", "1.12", "--userProperties", "{}"}, args));
+    }
+    
+    public static <T> T[] concat(T[] first, T[] second)
+    {
+        T[] result = Arrays.copyOf(first, first.length + second.length);
+        System.arraycopy(second, 0, result, first.length, second.length);
+        return result;
+    }
+}
