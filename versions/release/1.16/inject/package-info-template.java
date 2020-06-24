a1e89cf3ca9380401b2020cbfa821e4b601edc21
LexManos <LexManos@gmail.com>
2020-06-23 17:31:12 -0700

    1.16 release

---
+++ versions/release/1.16/inject/package-info-template.java
@@ -0,0 +1,7 @@
+// Auto generated package-info by MCP
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+package {PACKAGE};
+
+import mcp.MethodsReturnNonnullByDefault;
+import javax.annotation.ParametersAreNonnullByDefault;
